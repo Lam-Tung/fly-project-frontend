5913e92e0990f578732ddd9fd25fee511a740f4c
brotato <lamtung.truong@gmail.com>
2023-07-08 19:54:58 +0200

    * navbar working * finished styling

--- src/routes/lib/Navbar.svelte
+++ src/routes/lib/Navbar.svelte
@@ -1,14 +1,15 @@
 <!-- SCRIPT -->
 <script lang="ts">
     import { _ } from 'svelte-i18n';
+    import { page } from '$app/stores';
 </script>
 
 
 <!-- HTML -->
 <nav>
     <a href="/" class="project-name">{$_('general.project-name')}</a>
-    <a href="/">{$_('navbar.home')}</a>
-    <a href="/pokemon">{$_('navbar.pokemon')}</a>
+    <a href="/" class:active={$page.url.pathname === '/'}>{$_('navbar.home')}</a>
+    <a href="/pokemon" class:active={$page.url.pathname === '/pokemon'}>{$_('navbar.pokemon')}</a>
 </nav>
 
 
@@ -27,25 +28,53 @@
         a:first-child {
             margin: $spacing-24;
             color: $orange;
-            border: none;
+            border: $border-width solid transparent;
+            border-right: transparent;
 
             &:hover {
-                border: none;
+                border: $border-width solid transparent;
+                border-right: transparent;
+            }
+
+            &.active {
+                border: $border-width solid transparent;
+                border-right: transparent;
+                background-color: $dark-1;
             }
         }
         
         a {
             display: flex;
-            margin: 0 $spacing-24 $spacing-24 $spacing-24;
+            border: $border-width solid transparent;
+            border-right: transparent;
+            margin: 0 0 $spacing-24 $spacing-24;
             padding: $spacing-12;
             color: $light-1;
             text-decoration: none;
             font-weight: 600;
         }
 
+        a.active {
+            border: $border-width solid $light-1;
+            border-radius: $border-radius;
+            border-right: transparent;
+            border-top-right-radius: 0;
+            border-bottom-right-radius: 0;
+            background-color: $light-2;
+            color: $dark-1;
+
+            &:hover {
+                margin: 0 0 $spacing-24 $spacing-24;
+                border-right: transparent;
+                border-top-right-radius: 0;
+                border-bottom-right-radius: 0;
+            }
+        }
+
         a:hover {
             border: $border-width solid $light-1;
             border-radius: $border-radius;
+            margin: 0 $spacing-24 $spacing-24 $spacing-24;
         }
 
         .project-name {
